10de2097adc1ab8ca95faf712ff1c22a114d3dff
Thieso <mail@thiesoelerich.com>
2020-03-27 22:57:30 +0100

    picked up the project again, fixed the issue that the snake is one element too short, added some comments, fixed indentation

--- include/Snake.h
+++ include/Snake.h
@@ -5,21 +5,21 @@
 using namespace std; 
 
 class Snake {
-		private: 
-				vector<sf::RectangleShape> elements; 
-				vector<sf::Vector2f> newPos; 
-				unsigned int length; 
-				sf::RenderWindow* win; 
-				unsigned const int xSize = 20; 
-				unsigned const int ySize = 20; 
-				const int step = 20;
+    private: 
+        vector<sf::RectangleShape> elements; // elements of the snake
+        vector<sf::Vector2f> newPos;         // new positions of the elements after moving
+        unsigned int length;                 // length of the snake
+        sf::RenderWindow* win;               // window to draw in
+        unsigned const int xSize = 20;       // size of each element in x direction
+        unsigned const int ySize = 20;       // size of each element in y direction
+        const int step = 20;                 // step size of the snake
 
-		public: 
-				Snake(sf::RenderWindow*); 
-				~Snake(); 
-				unsigned int getLength(); 
-				sf::Vector2f getHead(); 
-				void addElement(); 
-				void moveSnake(const int, const int); 
-				int checkCollision(); 
+    public: 
+        Snake(sf::RenderWindow*); 
+        ~Snake(); 
+        unsigned int getLength(); 
+        sf::Vector2f getHead(); 
+        void addElement(); 
+        void moveSnake(const int, const int); 
+        int checkCollision(); 
 };
--- main.cpp
+++ main.cpp
@@ -6,152 +6,153 @@
 
 using namespace std;
 
+
 void drawMatrix(sf::RenderWindow*);
 
 sf::RenderWindow* window = new  sf::RenderWindow(sf::VideoMode(500, 500), "Snake");
 
 struct direction {
-		int x;
-		int y;
+    int x;
+    int y;
 };
 
 int main(){
 
-		// create a new Snake object
-		Snake* snake = new Snake(window);
-		
-
-		// create the direction struct with initial moving to the right
-		struct direction dir;
-		dir.x = 1;
-		dir.y = 0;
-
-		// set framerate
-		window->setFramerateLimit(30);
-
-		// set speed
-		int speed = 50;
-
-		// create clock
-		sf::Clock clock;
-
-		// variable for food checking
-		int avaiable = 0;
-
-		// random location for food
-		int randX, randY;
-
-		// create the food
-		Food* food = new Food(window);
-
-		// var for game over
-		int gameOver = 0;
-
-		// run program as long as the window is open
-		while (window->isOpen() && gameOver == 0) {
-				sf::Event event;
+    // create a new Snake object
+    Snake* snake = new Snake(window);
 
 
-				while(window->pollEvent(event)) {
-						// "close requested" event: we close the window
-						if (event.type == sf::Event::Closed)
-								window->close();
-				}
+    // create the direction struct with initial moving to the right
+    struct direction dir;
+    dir.x = 1;
+    dir.y = 0;
+
+    // set framerate
+    window->setFramerateLimit(30);
+
+    // set speed
+    int speed = 50;
+
+    // create clock
+    sf::Clock clock;
+
+    // variable for food checking
+    int avaiable = 0;
+
+    // random location for food
+    int randX, randY;
+
+    // create the food
+    Food* food = new Food(window);
+
+    // var for game over
+    int gameOver = 0;
+
+    // run program as long as the window is open
+    while (window->isOpen() && gameOver == 0) {
+        sf::Event event;
 
 
-				// get keyboard input
-				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && dir.x == 0) {
-						dir.x = -1;
-						dir.y = 0;
-				}
-				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && dir.x == 0) {
-						dir.x = 1;
-						dir.y = 0;
-				}
-				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && dir.y == 0) {
-						dir.x = 0;
-						dir.y = -1;
-				}
-				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && dir.y == 0) {
-						dir.x = 0;
-						dir.y = 1;
-				}
-				// if a certain time elapsed, move the snake
-				if (clock.getElapsedTime().asMilliseconds() >= speed){
-						window->clear();
-						clock.restart();
-						snake->moveSnake(dir.x, dir. y);
-				}
+        while(window->pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+
+
+        // get keyboard input
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && dir.x == 0) {
+            dir.x = -1;
+            dir.y = 0;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && dir.x == 0) {
+            dir.x = 1;
+            dir.y = 0;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && dir.y == 0) {
+            dir.x = 0;
+            dir.y = -1;
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && dir.y == 0) {
+            dir.x = 0;
+            dir.y = 1;
+        }
+        // if a certain time elapsed, move the snake
+        if (clock.getElapsedTime().asMilliseconds() >= speed){
+            window->clear();
+            clock.restart();
+            snake->moveSnake(dir.x, dir. y);
+        }
 
 
 
-				// place the food
-				if (avaiable == 0){
-						avaiable = 1;
-						 /* initialize random seed: */
-						srand (time(NULL));
+        // place the food
+        if (avaiable == 0){
+            avaiable = 1;
+            /* initialize random seed: */
+            srand (time(NULL));
 
 
-						/* generate secret number between 1 and 10: */
-						randX = rand() % 24;
-						randY = rand() % 24;
-						randX *= 20;
-						randY *= 20;
+            // generate secret number between 1 and 10:
+            randX = rand() % 24;
+            randY = rand() % 24;
+            randX *= 20;
+            randY *= 20;
 
-						food->setPos(randX, randY);
-				}
+            food->setPos(randX, randY);
+        }
 
-				// check if food got eaten
-				if (avaiable == 1 && snake->getHead() == sf::Vector2f(randX, randY)) {
-						avaiable = 0;
-						snake->addElement();
-				}
-
-
-				// check for collision
-				if (snake->checkCollision() == 1) {
-						gameOver = 1;
-				}
-
-				//DEBUG
-				//sf::Vector2f h = snake->getHead();
-				//cout << "snake at: x = " << h.x << " y = " << h.y << endl;
-				//food
-
-				// draw a Matrix
-				drawMatrix(window);
-
-				// draw the food
-				food->drawFood();
-
-				// Display all the changes on the screen
-				window->display();
-		}
+        // check if food got eaten
+        if (avaiable == 1 && snake->getHead() == sf::Vector2f(randX, randY)) {
+            avaiable = 0;
+            snake->addElement();
+        }
 
 
-		delete food;
-		return 0;
+        // check for collision
+        if (snake->checkCollision() == 1) {
+            gameOver = 1;
+        }
+
+        //DEBUG
+        //sf::Vector2f h = snake->getHead();
+        //cout << "snake at: x = " << h.x << " y = " << h.y << endl;
+        //food
+
+        // draw a Matrix
+        drawMatrix(window);
+
+        // draw the food
+        food->drawFood();
+
+        // Display all the changes on the screen
+        window->display();
+    }
+
+
+    delete food;
+    return 0;
 }
 
 void drawMatrix(sf::RenderWindow* window) {
 
-		int grey = 50;
-		bool x = true; 
+    int grey = 50;
+    bool x = true; 
 
-		sf::Color color(grey, grey, grey);
+    sf::Color color(grey, grey, grey);
 
-		for (int i = 20; i < 800; i += 20){
-				sf::Vertex lineV[] =
-				{
-					sf::Vertex(sf::Vector2f(i, 0), color),
-					sf::Vertex(sf::Vector2f(i, 800), color)
-				};
-				window->draw(lineV, 2, sf::Lines);
-				sf::Vertex lineH[] =
-				{
-					sf::Vertex(sf::Vector2f(0, i), color),
-					sf::Vertex(sf::Vector2f(800, i),color)
-				};
-				window->draw(lineH, 2, sf::Lines);
-		}
+    for (int i = 20; i < 800; i += 20){
+        sf::Vertex lineV[] =
+        {
+            sf::Vertex(sf::Vector2f(i, 0), color),
+            sf::Vertex(sf::Vector2f(i, 800), color)
+        };
+        window->draw(lineV, 2, sf::Lines);
+        sf::Vertex lineH[] =
+        {
+            sf::Vertex(sf::Vector2f(0, i), color),
+            sf::Vertex(sf::Vector2f(800, i),color)
+        };
+        window->draw(lineH, 2, sf::Lines);
+    }
 }
