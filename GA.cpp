cb07e7aad19bb38202bf80285d031810b7830e4b
Thieso <mail@thiesoelerich.com>
2020-04-28 19:59:35 +0200

    Finished implementation of the selection function, didn't have time to test it yet

--- GA.cpp
+++ GA.cpp
@@ -9,49 +9,49 @@ GA::GA() {
     nn = new NN(6, 4, 6);
     for (int i = 0; i < population_size; ++i) {
         Individual* ind = new Individual(6, 4, 6);
-        individuals.push_back(*ind);
+        population.push_back(*ind);
     }
 }
 
 // evaluate the fitness of all individuals in the current population
 vector<int> GA::evaluate_fitness() {
-    // iterate over the individuals and compute their fitness
+    // iterate over the population and compute their fitness
     for (int i = 0; i < population_size; ++i) {
-        current_fitness[i] = individuals[i].evaluate_fitness(nn);
+        current_fitness[i] = population[i].evaluate_fitness(nn);
     }
     return current_fitness;
 }
 
 // show the game on the window for individual with id id_individual
 void GA::show_game(sf::RenderWindow* window, int id_individual) {
-    individuals[id_individual].show_game(window, nn);
+    population[id_individual].show_game(window, nn);
 }
 
 // select new parents from the existing population based on their fitness value
 void GA::selection() {
-    this->sort_population_by_fitness();
-}
-
-// sort the individuals in the current population based on their fitness values
-void GA::sort_population_by_fitness() {
-    // create pair array for the population
-    pair<int, Individual> pairt[population_size]; 
-  
-    // Storing the respective array 
-    // elements in pairs. 
-    for (int i = 0; i < population_size; i++) { 
-        pairt[i].first = current_fitness[i];
-        pairt[i].second = individuals[i]; 
-    } 
-  
-    // Sorting the pair array. 
-    sort(pairt, pairt + population_size); 
-      
-    // Modifying original arrays 
-    for (int i = 0; i < population_size; i++) { 
-        current_fitness[i] = pairt[i].first; 
-        individuals[i] = pairt[i].second; 
-    } 
+    // compute total fitness
+    float total_fitness = accumulate(current_fitness.begin(), current_fitness.end(), 0);
+    // find the size of the roulette wheel part of each individual
+    vector<float> roulette_wheel(population_size);
+    for (int i = 0; i < population_size; i++) {
+        roulette_wheel[i] = ((float) current_fitness[i]) / total_fitness;
+        if (i > 0) {
+            roulette_wheel[i] = roulette_wheel[i] + roulette_wheel[i - 1];
+        }
+    }
+    // select the parents 
+    float wheel_spin;
+    for (int i = 0; i < nr_parents; i++) {
+        // create random number between 0 and 1
+        wheel_spin = ((float) (rand() % 100 + 1)) / 100.0;
+        // look where the wheel stops and select the parent
+        for (int j = 0; j < population_size; j++) {
+            if (wheel_spin > roulette_wheel[j] && wheel_spin < roulette_wheel[j+1]) {
+                parents[i] = population[j];
+                break;
+            }
+        }
+    }
 }
 
 // apply crossover to the existing population
--- include/GA.h
+++ include/GA.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <map>
+#include <numeric>
 #include "include/Individual.h"
 
 using namespace std; 
@@ -12,14 +14,14 @@ class GA {
         int nr_parents; // number of parents in each generation
         float mutation_rate; // rate of muations occuring during variation
         NN* nn; // neural network 
-        vector<Individual> individuals; // vector of individuals
+        vector<Individual> population; // vector of individuals making up the population
+        vector<Individual> parents; // vector of individuals making up the parents
         vector<int> current_fitness; // current fitness of the individuals
 
     public: 
         GA();
         vector<int> evaluate_fitness();
         void show_game(sf::RenderWindow* window, int id_individual);
-        void sort_population_by_fitness();
         void selection();
         void crossover();
         void mutation();
